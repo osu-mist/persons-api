afe33f16823a7bac5e59054da29b8d72b666460a
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-03-05 13:51:58 -0800

    Fix typo

--- src/main/groovy/edu/oregonstate/mist/personsapi/PersonsResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/personsapi/PersonsResource.groovy
@@ -41,7 +41,7 @@ class PersonsResource extends Resource {
 
         def id = [onid, osuID, osuUID].findAll { it }
         if (id.size() != 1) {
-            badRequest('onid, osuID, and osuUID cannot not be included together').build()
+            badRequest('onid, osuID, and osuUID cannot be included together').build()
         } else {
             def persons = personsDAO.getPersons(onid, osuID, osuUID)
             ResultObject res = new ResultObject(
