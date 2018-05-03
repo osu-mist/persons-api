419cba90eff210ffea8a9e03c596d57f6acc3595
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-05-03 13:22:27 -0700

    update swagger and job object POGO

--- src/main/groovy/edu/oregonstate/mist/personsapi/core/JobObject.groovy
+++ src/main/groovy/edu/oregonstate/mist/personsapi/core/JobObject.groovy
@@ -3,14 +3,44 @@ package edu.oregonstate.mist.personsapi.core
 import com.fasterxml.jackson.annotation.JsonFormat
 
 class JobObject {
+    String positionNumber
+    String suffix
+
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone="UTC")
     Date beginDate
 
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone="UTC")
     Date endDate
 
-    String positionNumber
+    String locationID
+    Integer appointmentBasis
     String status
     String description
-    Float fte
+    BigDecimal fte
+    Integer appointmentPercent
+
+    String supervisorID
+    String supervisorPositionNumber
+    String homeOrganizationCode
+    BigDecimal hourlyRate
+    BigDecimal hoursPerPay
+    BigDecimal assignmentSalary
+    Integer paysPerYear
+    BigDecimal annualSalary
+    BigDecimal appointmentSalary
+    String step
+    List<LaborDistribution> laborDistribution
+}
+
+class LaborDistribution {
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone="UTC")
+    Date effectiveDate
+
+    String chartOfAccountsCode
+    String accountIndex
+    String fund
+    String organizationCode
+    String accountCode
+    String programCode
+    BigDecimal distributionPercentage
 }
