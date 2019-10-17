0dd085ed91ca53ead2ee7096c36bbdf9059ee8ab
Alex Ruef <alex.ruef@oregonstate.edu>
2019-10-17 14:52:19 -0700

    added fields to PhoneMapper

--- src/main/groovy/edu/oregonstate/mist/personsapi/mapper/PhoneMapper.groovy
+++ src/main/groovy/edu/oregonstate/mist/personsapi/mapper/PhoneMapper.groovy
@@ -8,10 +8,15 @@ import java.sql.ResultSet
 import java.sql.SQLException
 
 public class PhoneMapper implements ResultSetMapper<PhoneObject> {
-  /* public PhoneObject map(int i, ResultSet rs, StatementContext sc) throws SQLException {
+  public PhoneObject map(int i, ResultSet rs, StatementContext sc) throws SQLException {
     new PhoneObject(
       id: rs.getString('SPRTELE_PIDM'),
-      areaCode: rs.getString('SPRTELE_PHONE_AREA')
+      areaCode: rs.getString('AREA_CODE'),
+      phoneNumber: rs.getString('PHONE_NUMBER'),
+      phoneExtension: rs.getString('PHONE_EXTENSION'),
+      phoneCode: rs.getString('PHONE_CODE'),
+      intlAccess: rs.getString('INTL_ACCESS'),
+      activityDate: rs.getString('ACTIVITY_DATE'),
     )
-  } */
+  }
 }
