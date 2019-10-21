092e4d4b793d79d7c799c3dd860625a81da05adb
Alex Ruef <alex.ruef@oregonstate.edu>
2019-10-21 10:32:36 -0700

    corrected class name on PhoneRecordMapper

--- src/main/groovy/edu/oregonstate/mist/personsapi/mapper/phoneRecordMapper.groovy
+++ src/main/groovy/edu/oregonstate/mist/personsapi/mapper/phoneRecordMapper.groovy
@@ -7,7 +7,7 @@ import org.skife.jdbi.v2.tweak.ResultSetMapper
 import java.sql.ResultSet
 import java.sql.SQLException
 
-public class PhoneMapper implements ResultSetMapper<PhoneRecordObject> {
+public class PhoneRecordMapper implements ResultSetMapper<PhoneRecordObject> {
   public PhoneRecordObject map(int i, ResultSet rs, StatementContext sc) throws SQLException {
     new PhoneRecordObject(
       addressSeqno: rs.getString('SPRTELE_ADDR_SEQNO'),
