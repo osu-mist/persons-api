6f3621342d357164d28dbb24c7705bd1dab74274
Alex Ruef <alex.ruef@oregonstate.edu>
2020-02-12 15:07:09 -0800

    added errorHandler for person endpoint

--- src/api/v2/paths/persons.js
+++ src/api/v2/paths/persons.js
@@ -4,9 +4,13 @@ import { getPerson } from '../db/oracledb/persons-dao';
 
 const get = async (req, res) => {
   console.log('get person endpoint');
-  const { osuId } = req.query;
-  const serializedPerson = await getPerson(osuId[0]);
-  return res.send(serializedPerson);
+  try {
+    const { osuId } = req.query;
+    const result = await getPerson(osuId[0]);
+    return res.send(result);
+  } catch (err) {
+    return errorHandler(res, err);
+  }
 };
 
 const post = async (req, res) => {
