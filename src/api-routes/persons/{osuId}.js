2c1ca16e3440efcc93f66edca19627eeac392277
Alex Ruef <alex.ruef@oregonstate.edu>
2020-03-13 14:25:10 -0700

    capitalize OSU

--- src/api-routes/persons/{osuId}.js
+++ src/api-routes/persons/{osuId}.js
@@ -13,7 +13,7 @@ const get = async (req, res) => {
     const { params: binds } = req;
     const result = await getPersonById(binds);
     if (!result) {
-      return errorBuilder(res, 404, 'A person with the specified osu ID was not found.');
+      return errorBuilder(res, 404, 'A person with the specified OSU ID was not found.');
     }
     return res.send(result);
   } catch (err) {
