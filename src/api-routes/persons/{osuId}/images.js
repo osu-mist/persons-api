4fa892aeedacd7ef66fdad033012504bc338306b
Alex Ruef <ruef.alex@gmail.com>
2020-03-20 09:54:18 -0700

    added resizing for images that are too large

--- src/api-routes/persons/{osuId}/images.js
+++ src/api-routes/persons/{osuId}/images.js
@@ -18,14 +18,19 @@ const get = async (req, res) => {
     if (!image) {
       return errorBuilder(res, 404, 'No image with the specified OSU ID was not found.');
     }
-    res.contentType('image/jpeg');
 
+    res.contentType('image/jpeg');
+    let result;
+    const sharpImage = sharp(image);
     if (width) {
-      const resizedImage = await sharp(image).resize(width, null).toBuffer();
-      return res.send(resizedImage);
+      result = await sharpImage.resize(width, null).toBuffer();
+    } else if (await sharpImage.metadata().width > 2000) {
+      result = await sharpImage.resize(2000, null).toBuffer();
+    } else {
+      result = image;
     }
 
-    return res.send(image);
+    return res.send(result);
   } catch (err) {
     return errorHandler(res, err);
   }
