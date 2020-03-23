69cc7ddfc868f1f1be4b14e336c496a7334c3aa4
Alex Ruef <ruef.alex@gmail.com>
2020-03-23 13:51:25 -0700

    initial commit for phones path

---
+++ src/api-routes/persons/{osuId}/phones.js
@@ -0,0 +1,30 @@
+import { errorHandler } from 'errors/errors';
+import { getPhones } from 'db/oracledb/phones-dao';
+
+/**
+ * Get phones by OSU ID
+ *
+ * @type {RequestHandler}
+ */
+const get = async (req, res) => {
+  try {
+    console.log('get phones endpoint');
+    const { osuId } = req.params;
+    const { query } = req.query;
+    const result = await getPhones(osuId, query);
+    return res.send(result);
+  } catch (err) {
+    return errorHandler(res, err);
+  }
+};
+
+/**
+ * Post phones endpoint
+ *
+ * @type {RequestHandler}
+ */
+const post = async () => {
+  // todo
+};
+
+export { get, post };
