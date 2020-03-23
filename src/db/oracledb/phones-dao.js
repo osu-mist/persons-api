4bd592e7d8fd1e599f47c1c7027a3c9add1f4269
Alex Ruef <ruef.alex@gmail.com>
2020-03-23 13:51:50 -0700

    initial commit for phones-dao

---
+++ src/db/oracledb/phones-dao.js
@@ -0,0 +1,26 @@
+import { parseQuery } from 'utils/parse-query';
+import { getConnection } from './connection';
+import { contrib } from './contrib/contrib';
+
+/**
+ * Queries data source for raw person data and passes it to the serializer
+ *
+ * @param {string} osuId OSU ID of person to select
+ */
+const getPhones = async (osuId, query) => {
+  const connection = await getConnection();
+  try {
+    console.log('getPhones dao');
+    const parsedQuery = parseQuery(query);
+    parsedQuery.osuId = osuId;
+    parsedQuery.phoneType = parsedQuery.phoneType ? parsedQuery.phoneType : null;
+    parsedQuery.addressType = parsedQuery.addressType ? parsedQuery.addressType : null;
+    const { rows } = await connection.execute(contrib.getPhones(), parsedQuery);
+    console.log(rows);
+    return undefined;
+  } finally {
+    connection.close();
+  }
+};
+
+export { getPhones };
