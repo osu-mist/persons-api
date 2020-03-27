b17b560957b890ff8d004310a0994f9a28ec0c51
Alex Ruef <alex.ruef@oregonstate.edu>
2020-03-27 16:26:55 -0700

    initial commit for meal plans dao

---
+++ src/db/oracledb/meal-plans-dao.js
@@ -0,0 +1,28 @@
+import { parseQuery } from 'utils/parse-query';
+import { getConnection } from './connection';
+import { contrib } from './contrib/contrib';
+
+/**
+ * Queries data source for raw meal-plan data
+ *
+ * @param {string} osuId OSU ID of person to select addresses from
+ * @param {object} query Query parameters passed in with request
+ * @returns {Promise<object>} Serialized address resource
+ */
+const getMealPlansByOsuId = async (osuId, query) => {
+  const connection = await getConnection('odsRead');
+  try {
+    const parsedQuery = parseQuery(query);
+    parsedQuery.osuId = osuId;
+
+    const { rows } = await connection.execute(
+      contrib.getMealPlansByOsuId(parsedQuery),
+      parsedQuery,
+    );
+    return rows;
+  } finally {
+    connection.close();
+  }
+};
+
+export { getMealPlansByOsuId };
