502d354c9d0147f8c28f5430fb087a1bb95ebec8
Alex Ruef <alex.ruef@oregonstate.edu>
2020-03-27 16:23:07 -0700

    initial commit for get meal-plans endpoint

---
+++ src/api-routes/persons/{osuId}/meal-plans.js
@@ -0,0 +1,26 @@
+import { errorHandler, errorBuilder } from 'errors/errors';
+import { getMealPlansByOsuId } from 'db/oracledb/meal-plans-dao';
+import { personExists } from 'db/oracledb/persons-dao';
+
+/**
+ * Get meal plan by OSU ID
+ *
+ * @type {RequestHandler}
+ */
+const get = async (req, res) => {
+  try {
+    const { query, params: { osuId } } = req;
+
+    if (!await personExists(osuId)) {
+      return errorBuilder(res, 404, 'A person with the specified OSU ID was not found.');
+    }
+
+    const result = await getMealPlansByOsuId(osuId, query);
+
+    return res.send(result);
+  } catch (err) {
+    return errorHandler(res, err);
+  }
+};
+
+export { get };
