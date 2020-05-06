bc829e3fd6fe4ed4c06e768d05159ae21a52788d
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-06 10:44:49 -0700

    Added employmentType filter

--- src/db/oracledb/jobs-dao.js
+++ src/db/oracledb/jobs-dao.js
@@ -16,13 +16,17 @@ const getJobs = async (internalId, query) => {
   try {
     const parsedQuery = parseQuery(query);
     parsedQuery.internalId = internalId;
+    const binds = _.omit(parsedQuery, ['employmentType']);
 
-    const { rows } = await connection.execute(contrib.getJobs(parsedQuery), parsedQuery);
+    const { rows } = await connection.execute(contrib.getJobs(parsedQuery), binds);
 
     await _.asyncEach(rows, async (row) => {
-      const binds = await _.pick(row, ['positionNumber', 'suffix']);
-      binds.internalId = internalId;
-      const { rows: labors } = await connection.execute(contrib.getLaborDistribution(), binds);
+      const labourBinds = await _.pick(row, ['positionNumber', 'suffix']);
+      labourBinds.internalId = internalId;
+      const { rows: labors } = await connection.execute(
+        contrib.getLaborDistribution(),
+        labourBinds,
+      );
       row.laborDistribution = labors;
     });
 
