76bd42268eef8508df6784f65430b3254ed94ccb
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-07 12:28:04 -0700

    Added check for birthDate

--- src/api-routes/persons.js
+++ src/api-routes/persons.js
@@ -11,6 +11,10 @@ const post = async (req, res) => {
   try {
     const { body: { data: { attributes } } } = req;
 
+    if (new Date(attributes.birthDate) > new Date()) {
+      return errorBuilder(res, 400, ["birthDate can't be a future date"]);
+    }
+
     const result = await createPerson(attributes);
 
     if (result instanceof Error) {
