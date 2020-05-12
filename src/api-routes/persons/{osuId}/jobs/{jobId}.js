7125d7dd589181f55abb6b209bc0a44152113d81
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-12 16:45:58 -0700

    Added patch handler for patch jobs

--- src/api-routes/persons/{osuId}/jobs/{jobId}.js
+++ src/api-routes/persons/{osuId}/jobs/{jobId}.js
@@ -1,4 +1,4 @@
-import { getJobByJobId } from 'db/oracledb/jobs-dao';
+import { getJobByJobId, createOrUpdateJob } from 'db/oracledb/jobs-dao';
 import { personExists } from 'db/oracledb/persons-dao';
 import { errorHandler, errorBuilder } from 'errors/errors';
 import { serializeJob } from 'serializers/jobs-serializer';
@@ -29,8 +29,36 @@ const get = async (req, res) => {
   }
 };
 
-const patch = () => {
-  // todo
+/**
+ * Patch jobs endpoint
+ *
+ * @type {RequestHandler}
+ */
+const patch = async (req, res) => {
+  try {
+    const { body, params: { osuId, jobId } } = req;
+
+    const internalId = await personExists(osuId);
+    if (!internalId) {
+      return errorBuilder(res, 404, 'A person with the specified OSU ID was not found.');
+    }
+
+    const jobExists = await getJobByJobId(internalId, jobId);
+    if (!jobExists) {
+      return errorBuilder(res, 404, 'A job with the specified job ID was not found.');
+    }
+
+    const result = await createOrUpdateJob(true, osuId, body.data.attributes, internalId);
+
+    if (result instanceof Error) {
+      return errorBuilder(res, 400, [result.message]);
+    }
+
+    const serializedJob = serializeJob(result, osuId);
+    return res.send(serializedJob);
+  } catch (err) {
+    return errorHandler(res, err);
+  }
 };
 
 export { get, patch };
