4100785277022f52f2aa4140d62f17d60b75cf45
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-26 16:04:48 -0700

    Finished tests for person-dao

--- src/tests/unit/persons-dao.test.js
+++ src/tests/unit/persons-dao.test.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import _ from 'lodash';
 import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 
@@ -13,19 +14,65 @@ describe('Test persons-dao', () => {
 
   const fakeOsuId = '999999999';
 
-  const createDaoProxy = () => proxyquire('db/oracledb/persons-dao', {
+  const createDaoProxy = (dbReturn) => proxyquire('db/oracledb/persons-dao', {
     './connection': {
       getConnection: sinon.stub().resolves({
-        execute: () => ({ rows: [{}] }),
+        execute: () => dbReturn,
         close: () => null,
         commit: () => null,
+        rollback: () => null,
       }),
     },
   });
 
-  it('getPersonById should return single result', async () => {
-    const daoProxy = createDaoProxy();
-    const result = daoProxy.getPersonById(fakeOsuId);
-    result.should.eventually.be.fulfilled.and.deep.equal({});
+  const testCases = [
+    {
+      message: 'getPersonById should return single result',
+      functionName: 'getPersonById',
+      expected: {},
+      dbReturn: { rows: [{}] },
+    },
+    {
+      message: 'personExists should return ID when person exists',
+      functionName: 'personExists',
+      expected: fakeOsuId,
+      dbReturn: { rows: [{ internalId: fakeOsuId }] },
+    },
+    {
+      message: 'personExists should return null for non-existent person',
+      functionName: 'personExists',
+      expected: null,
+      dbReturn: { rows: {} },
+    },
+    {
+      message: 'createPerson should return person data on success',
+      functionName: 'createPerson',
+      expected: { firstName: 'sally' },
+      dbReturn: { outBinds: { outId: fakeOsuId }, rows: [{ firstName: 'sally' }] },
+    },
+  ];
+  _.forEach(testCases, ({
+    message,
+    functionName,
+    expected,
+    dbReturn,
+  }) => {
+    it(message, () => {
+      const daoProxy = createDaoProxy(dbReturn);
+      const result = daoProxy[functionName]({ fakeOsuId });
+      return result.should.eventually.be.fulfilled.and.deep.equal(expected);
+    });
+  });
+
+  it('createPerson should throw an error when outId contains an error', () => {
+    const daoProxy = createDaoProxy({ outBinds: { outId: fakeOsuId }, rows: [null] });
+    const result = daoProxy.createPerson({});
+    return result.should.eventually.be.rejectedWith('Person creation failed');
+  });
+
+  it('createPerson should return error when outId contains ERROR', () => {
+    const daoProxy = createDaoProxy({ outBinds: { outId: 'ERROR: something happened' } });
+    const result = daoProxy.createPerson({});
+    return result.should.eventually.be.a('error');
   });
 });
