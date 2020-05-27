d05533a7ec91f87431155a1a2a91040ea482271a
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-27 13:59:09 -0700

    Finished tests for images-dao

--- src/tests/unit/images-dao.test.js
+++ src/tests/unit/images-dao.test.js
@@ -13,5 +13,41 @@ chai.use(chaiAsPromised);
 describe('Test images-dao', () => {
   sinon.stub(logger, 'error').returns(null);
 
+  const createDaoProxy = (dbReturn) => proxyquire('db/oracledb/images-dao', {
+    './connection': {
+      getConnection: sinon.stub().resolves({
+        execute: () => dbReturn,
+        close: () => null,
+        commit: () => null,
+        rollback: () => null,
+      }),
+    },
+  });
+
+  const testCases = [
+    {
+      message: 'getImageById should return image data on success',
+      dbReturn: { rows: [{ image: 'image data' }] },
+      expected: 'image data',
+    },
+    {
+      message: 'getImageById should return undefined when rows is undefined',
+      dbReturn: {},
+      expected: undefined,
+    },
+    {
+      message: 'getImageById should return undefined when rows is empty',
+      dbReturn: { rows: [] },
+      expected: undefined,
+    },
+  ];
+  _.forEach(testCases, ({ message, dbReturn, expected }) => {
+    it(message, () => {
+      const daoProxy = createDaoProxy(dbReturn);
+      const result = daoProxy.getImageById(fakeOsuId);
+      return result.should.eventually.be.fulfilled.and.deep.equal(expected);
+    });
+  });
+
   sinon.restore();
 });
