9858cb21fd1b1ca31123580006cf8f5798cbb5f0
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-28 09:31:47 -0700

    Added test

--- src/tests/unit/addresses-dao.test.js
+++ src/tests/unit/addresses-dao.test.js
@@ -3,18 +3,23 @@ import chaiAsPromised from 'chai-as-promised';
 // import _ from 'lodash';
 import sinon from 'sinon';
 
-
 import { logger } from 'utils/logger';
 import { fakeOsuId } from './mock-data';
-// import { createDaoProxy } from './test-helpers';
+import { createDaoProxy } from './test-helpers';
 
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('Test persons-dao', () => {
+describe('Test addresses-dao', () => {
+  const daoPath = '../../db/oracledb/addresses-dao';
+
   sinon.stub(logger, 'error').returns(null);
 
-  console.log(fakeOsuId);
+  it('getAddressesByInternalId should return multiple results', () => {
+    const daoProxy = createDaoProxy(daoPath, { rows: [{}, {}] });
+    const result = daoProxy.getAddressesByInternalId(fakeOsuId, {});
+    return result.should.eventually.be.fulfilled.and.deep.equal([{}, {}]);
+  });
 
   sinon.restore();
 });
