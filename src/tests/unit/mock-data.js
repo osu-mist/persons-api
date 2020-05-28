1d85084c6ec8b2225cea3c8f4d6c86e66f500386
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-28 14:21:16 -0700

    Added test data for addresses-serializer tests

--- src/tests/unit/mock-data.js
+++ src/tests/unit/mock-data.js
@@ -51,9 +51,30 @@ const serializedPerson = {
   },
 };
 
+const rawAddress = {
+  addressId: 'cf4c689f-94d4-42df-af59-9934cccdc52e',
+  'addressType.code': 'PA',
+  'addressType.description': 'Student Alternate Contact',
+  addressLine1: '188 W Jadon Dr',
+  addressLine2: null,
+  addressLine3: null,
+  addressLine4: null,
+  houseNumber: null,
+  city: 'Lebanon',
+  stateCode: 'OR',
+  state: 'Oregon',
+  postalCode: '97355-1686',
+  countyCode: '41043',
+  county: 'Linn',
+  nationCode: null,
+  nation: null,
+  lastModified: '2015-10-19',
+};
+
 export {
   fakeOsuId,
   fakeBaseUrl,
   rawPerson,
   serializedPerson,
+  rawAddress,
 };
