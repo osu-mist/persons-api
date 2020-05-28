852f3ae910946c61cc5beca7b40b989a4e1b4d54
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-28 14:31:09 -0700

    Added serializer test helpers

--- src/tests/unit/test-helpers.js
+++ src/tests/unit/test-helpers.js
@@ -1,5 +1,9 @@
+import { expect } from 'chai';
+import _ from 'lodash';
 import proxyquire from 'proxyquire';
 import sinon from 'sinon';
+
+import { fakeBaseUrl, fakeOsuId } from './mock-data';
 
 /**
  * Creates a proxy for the dao file being tested
@@ -18,4 +22,62 @@ const createDaoProxy = (daoPath, dbReturn) => proxyquire(daoPath, {
   },
 });
 
-export { createDaoProxy };
+/**
+ *
+ * @param {string} resourceType type of resource as named in openapi
+ * @param {object} resourceAttributes fields expected in attributes subset of resourceType
+ */
+const resourceSubsetSchema = (resourceType, resourceAttributes) => {
+  const fakeUrl = `${fakeBaseUrl}/${resourceType}s/fakeOsuId`;
+  const schema = {
+    links: {
+      self: fakeUrl,
+    },
+    data: {
+      id: fakeOsuId,
+      type: resourceType,
+      links: { self: fakeUrl },
+    },
+  };
+  if (resourceAttributes) {
+    schema.data.attributes = resourceAttributes;
+  }
+  return schema;
+};
+
+/**
+ * Helper function for lite-testing single resource
+ *
+ * @param {object} serializedResource serialized resource
+ * @param {string} resourceType resource type
+ * @param {object} nestedProps object containing properties nested under data.attributes
+ */
+const testSingleResource = (serializedResource, resourceType, nestedProps) => {
+  expect(serializedResource).to.have.all.keys(resourceSubsetSchema(resourceType, nestedProps));
+
+  if (nestedProps) {
+    _.forEach(Object.keys(nestedProps), (prop) => {
+      expect(serializedResource).to.have.nested.property(`data.attributes.${prop}`);
+    });
+  }
+};
+
+/**
+ * Helper function for lite-testing multiple resources
+ *
+ * @param {object} serializedResources serialized resources
+ * @returns {object} data object from serialized resources for further use
+ */
+const testMultipleResources = (serializedResources) => {
+  const serializedResourcesData = serializedResources.data;
+  expect(_.omit(serializedResources, 'meta')).to.have.keys('data', 'links');
+  expect(serializedResourcesData).to.be.an('array');
+
+  return serializedResourcesData;
+};
+
+export {
+  createDaoProxy,
+  testSingleResource,
+  testMultipleResources,
+};
