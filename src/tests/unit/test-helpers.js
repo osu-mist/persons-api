4a9030e986486203596dd33cdf835d53d6c336e5
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-28 10:32:41 -0700

    Seperated connection stub from dao proxy function

--- src/tests/unit/test-helpers.js
+++ src/tests/unit/test-helpers.js
@@ -2,6 +2,19 @@ import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 
 /**
+ * Creates sinon stub for oracledb connection
+ *
+ * @param {object} dbReturn value to be returned by connection.execute()
+ * @returns {Promise<object>} sinon stub for connection
+ */
+const getConnectionStub = (dbReturn) => sinon.stub().resolves({
+  execute: () => dbReturn,
+  close: () => null,
+  commit: () => null,
+  rollback: () => null,
+});
+
+/**
  * Creates a proxy for the dao file being tested
  *
  * @param {string} daoPath relative path to dao file
@@ -9,13 +22,8 @@ import sinon from 'sinon';
  */
 const createDaoProxy = (daoPath, dbReturn) => proxyquire(daoPath, {
   './connection': {
-    getConnection: sinon.stub().resolves({
-      execute: () => dbReturn,
-      close: () => null,
-      commit: () => null,
-      rollback: () => null,
-    }),
+    getConnection: getConnectionStub(dbReturn),
   },
 });
 
-export { createDaoProxy };
+export { createDaoProxy, getConnectionStub };
