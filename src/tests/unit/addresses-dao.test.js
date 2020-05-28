3a914b24b466ce15017d4f409407ec20284526d2
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-28 10:33:15 -0700

    Added success cases for addresses-dao tests

--- src/tests/unit/addresses-dao.test.js
+++ src/tests/unit/addresses-dao.test.js
@@ -1,11 +1,10 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-// import _ from 'lodash';
+import _ from 'lodash';
 import sinon from 'sinon';
 
 import { logger } from 'utils/logger';
-import { fakeOsuId } from './mock-data';
-import { createDaoProxy } from './test-helpers';
+import { createDaoProxy, getConnectionStub } from './test-helpers';
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -15,10 +14,44 @@ describe('Test addresses-dao', () => {
 
   sinon.stub(logger, 'error').returns(null);
 
-  it('getAddressesByInternalId should return multiple results', () => {
-    const daoProxy = createDaoProxy(daoPath, { rows: [{}, {}] });
-    const result = daoProxy.getAddressesByInternalId(fakeOsuId, {});
-    return result.should.eventually.be.fulfilled.and.deep.equal([{}, {}]);
+  const testCases = [
+    {
+      message: 'getAddressesByInternalId should return multiple results',
+      functionName: 'getAddressesByInternalId',
+      dbReturn: { rows: [{}, {}] },
+      expected: [{}, {}],
+    },
+    {
+      message: 'hasSameAddressType should return single result on success',
+      functionName: 'hasSameAddressType',
+      dbReturn: { rows: [{}] },
+      expected: {},
+    },
+    {
+      message: 'phoneHasSameAddressType should return single result on success',
+      functionName: 'phoneHasSameAddressType',
+      dbReturn: { rows: [{}] },
+      expected: {},
+    },
+    {
+      message: 'createAddress should return single result',
+      functionName: 'createAddress',
+      dbReturn: { outBinds: { seqno: {} }, rows: [{}] },
+      expected: { addrSeqno: {} },
+    },
+  ];
+  _.forEach(testCases, ({
+    message,
+    functionName,
+    dbReturn,
+    expected,
+  }) => {
+    it(message, async () => {
+      const daoProxy = createDaoProxy(daoPath, dbReturn);
+      const connectionStub = await getConnectionStub(dbReturn)();
+      const result = daoProxy[functionName](connectionStub, { addressType: {} });
+      return result.should.eventually.be.fulfilled.and.deep.equal(expected);
+    });
   });
 
   sinon.restore();
