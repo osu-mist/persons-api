3a79e0e3a053282fe8152d743525d0bd0dee5c25
Alex Ruef <alex.ruef@oregonstate.edu>
2020-05-29 08:59:30 -0700

    No longer need to use proxyquire in serializer tests

--- src/tests/unit/meal-plans-serializer.test.js
+++ src/tests/unit/meal-plans-serializer.test.js
@@ -1,29 +1,19 @@
 import chai from 'chai';
-import proxyquire from 'proxyquire';
 
+import { serializeMealPlan, serializeMealPlans } from 'serializers/meal-plans-serializer';
+import { rawMealPlan, fakeOsuId } from './mock-data';
 import { testSingleResource, testMultipleResources } from './test-helpers';
-
-import {
-  rawMealPlan,
-  fakeOsuId,
-} from './mock-data';
 
 chai.should();
 
 describe('Test meal-plans-serializer', () => {
-  // Using proxyquire to avoid issues with config in imported classes
-  let serialProxy;
-  beforeEach(() => {
-    serialProxy = proxyquire('serializers/meal-plans-serializer', {});
-  });
-
   it('serializeMealPlan should return singular data in proper JSON API format', () => {
-    const result = serialProxy.serializeMealPlan(rawMealPlan, fakeOsuId);
+    const result = serializeMealPlan(rawMealPlan, fakeOsuId);
     return testSingleResource(result, 'meal-plans');
   });
 
   it('serializeMealPlans should return multiple records in proper JSON API format', () => {
-    const result = serialProxy.serializeMealPlans([rawMealPlan, rawMealPlan], fakeOsuId);
+    const result = serializeMealPlans([rawMealPlan, rawMealPlan], fakeOsuId);
     return testMultipleResources(result);
   });
 });
