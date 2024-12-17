2d6df0ccf36f9d2c3a04d787bf6b616ea9866099
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2024-12-17 15:05:12 -0800

    Fix

--- src/tests/integration/integration_test.py
+++ src/tests/integration/integration_test.py
@@ -1,4 +1,5 @@
 """Integration tests"""
+
 import json
 import logging
 import unittest
@@ -13,14 +14,13 @@ class IntegrationTests(utils.UtilsTestCase):
     @classmethod
     def setup(cls, config_path, openapi_path):
         """Performs basic setup"""
-
         with open(config_path) as config_file:
             config = json.load(config_file)
             cls.base_url = utils.setup_base_url(config)
             cls.session = utils.setup_session(config)
-            cls.test_cases = config['test_cases']
-            cls.local_test = config['local_test']
-            cls.query_params = config['query_params']
+            cls.test_cases = config["test_cases"]
+            cls.local_test = config["local_test"]
+            cls.query_params = config["query_params"]
 
         with open(openapi_path) as openapi_file:
             openapi = yaml.load(openapi_file, Loader=yaml.SafeLoader)
@@ -33,276 +33,240 @@ class IntegrationTests(utils.UtilsTestCase):
 
     def test_get_person_by_id(self):
         """Test case: GET /persons/{osuId}"""
-
-        endpoint = '/persons'
-        nullable_fields = self.get_nullable_fields('PersonResource')
-        valid_person_ids = self.test_cases['valid_person_ids']
-        invalid_osu_ids = self.test_cases['invalid_osu_ids']
+        endpoint = "/persons"
+        nullable_fields = self.get_nullable_fields("PersonResource")
+        valid_person_ids = self.test_cases["valid_person_ids"]
+        invalid_osu_ids = self.test_cases["invalid_osu_ids"]
 
         for person in valid_person_ids:
-            resource = 'PersonResource'
-            osu_id = person['osu_id']
+            resource = "PersonResource"
+            osu_id = person["osu_id"]
             self.check_endpoint(
-                f'{endpoint}/{osu_id}',
+                f"{endpoint}/{osu_id}",
                 resource,
                 200,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
         for osu_id in invalid_osu_ids:
-            resource = 'ErrorObject'
+            resource = "ErrorObject"
             self.check_endpoint(
-                f'{endpoint}/{osu_id}',
+                f"{endpoint}/{osu_id}",
                 resource,
                 404,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
     def test_get_job_by_id(self):
         """Test case: GET /persons/{osuId}/jobs/{jobId}"""
-
-        endpoint = '/jobs'
-        nullable_fields = self.get_nullable_fields('JobResource')
-        valid_person_ids = self.test_cases['valid_person_ids']
-        invalid_job_ids = self.test_cases['invalid_job_ids']
+        endpoint = "/jobs"
+        nullable_fields = self.get_nullable_fields("JobResource")
+        valid_person_ids = self.test_cases["valid_person_ids"]
+        invalid_job_ids = self.test_cases["invalid_job_ids"]
 
         for person in valid_person_ids:
-            resource = 'JobResource'
-            osu_id = person['osu_id']
-            job_id = person['job_id']
+            resource = "JobResource"
+            osu_id = person["osu_id"]
+            job_id = person["job_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}/{job_id}',
+                f"/persons/{osu_id}{endpoint}/{job_id}",
                 resource,
                 200,
                 nullable_fields=nullable_fields,
-                max_elapsed_seconds=11
+                max_elapsed_seconds=11,
             )
 
-        osu_id = invalid_job_ids['osu_id']
-        for job_id in invalid_job_ids['job_ids']:
+        osu_id = invalid_job_ids["osu_id"]
+        for job_id in invalid_job_ids["job_ids"]:
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}/{job_id}',
-                'ErrorObject',
+                f"/persons/{osu_id}{endpoint}/{job_id}",
+                "ErrorObject",
                 404,
                 nullable_fields=nullable_fields,
-                max_elapsed_seconds=11
+                max_elapsed_seconds=11,
             )
 
     def test_get_jobs(self):
         """Test case: GET /persons/{osuId}/jobs"""
+        endpoint = "/jobs"
+        nullable_fields = self.get_nullable_fields("JobResource")
+        valid_person_ids = self.test_cases["valid_person_ids"]
 
-        endpoint = '/jobs'
-        nullable_fields = self.get_nullable_fields('JobResource')
-        valid_person_ids = self.test_cases['valid_person_ids']
-
-        resource = 'JobResource'
+        resource = "JobResource"
         for person in valid_person_ids:
-            osu_id = person['osu_id']
+            osu_id = person["osu_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}',
+                f"/persons/{osu_id}{endpoint}",
                 resource,
                 200,
                 nullable_fields=nullable_fields,
-                max_elapsed_seconds=11
+                max_elapsed_seconds=11,
             )
 
-        query_params = self.query_params['jobs']
-        osu_id = self.query_params['osu_id']
+        query_params = self.query_params["jobs"]
+        osu_id = self.query_params["osu_id"]
         self.check_query_params(
-            f'/persons/{osu_id}{endpoint}',
+            f"/persons/{osu_id}{endpoint}",
             resource,
             nullable_fields,
             query_params,
-            osu_id
+            osu_id,
         )
 
     def test_get_images(self):
         """Test case: GET /persons/{osuId}/images"""
-
-        endpoint = '/images'
-        valid_person_ids = self.test_cases['valid_person_ids']
+        endpoint = "/images"
+        valid_person_ids = self.test_cases["valid_person_ids"]
 
         for person in valid_person_ids:
-            osu_id = person['osu_id']
+            osu_id = person["osu_id"]
 
             """
             check_endpoint assumes response will be in JSON format
             images returns image data so we can't use that method
             """
-            requested_url = f'{self.base_url}/persons/{osu_id}{endpoint}'
+            requested_url = f"{self.base_url}/persons/{osu_id}{endpoint}"
             response = self.session.get(requested_url)
             status_code = response.status_code
             self.assertEqual(
                 status_code,
                 200,
-                f'requested_url: {requested_url},\nresponse_body: {response}'
+                f"requested_url: {requested_url},\nresponse_body: {response}",
             )
 
     def test_get_meal_plans(self):
         """Test case: GET /persons/{osuId}/meal-plans"""
-
-        endpoint = '/meal-plans'
-        resource = 'MealPlanResource'
+        endpoint = "/meal-plans"
+        resource = "MealPlanResource"
         nullable_fields = self.get_nullable_fields(resource)
-        valid_meal_plan_ids = self.test_cases['valid_meal_plan_ids']
+        valid_meal_plan_ids = self.test_cases["valid_meal_plan_ids"]
 
         for meal_plan in valid_meal_plan_ids:
-            osu_id = meal_plan['osu_id']
+            osu_id = meal_plan["osu_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}',
+                f"/persons/{osu_id}{endpoint}",
                 resource,
                 200,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
-        query_params = self.query_params['meal_plans']
-        osu_id = self.query_params['osu_id']
+        query_params = self.query_params["meal_plans"]
+        osu_id = self.query_params["osu_id"]
         self.check_query_params(
-            f'/persons/{osu_id}{endpoint}',
+            f"/persons/{osu_id}{endpoint}",
             resource,
             nullable_fields,
             query_params,
-            osu_id
+            osu_id,
         )
 
     def test_get_meal_plans_by_meal_plan_id(self):
         """Test case: GET /persons/{osuId}/meal-plans/{mealPlanId}"""
-
-        endpoint = '/meal-plans'
-        resource = 'MealPlanResource'
+        endpoint = "/meal-plans"
+        resource = "MealPlanResource"
         nullable_fields = self.get_nullable_fields(resource)
-        valid_meal_plan_ids = self.test_cases['valid_meal_plan_ids']
+        valid_meal_plan_ids = self.test_cases["valid_meal_plan_ids"]
 
         for meal_plan in valid_meal_plan_ids:
-            osu_id = meal_plan['osu_id']
-            meal_plan_id = meal_plan['meal_plan_id']
+            osu_id = meal_plan["osu_id"]
+            meal_plan_id = meal_plan["meal_plan_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}/{meal_plan_id}',
+                f"/persons/{osu_id}{endpoint}/{meal_plan_id}",
                 resource,
                 200,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
-        invalid_meal_plan_ids = self.test_cases['invalid_meal_plan_ids']
-        osu_id = invalid_meal_plan_ids['osu_id']
-        for meal_plan_id in invalid_meal_plan_ids['meal_plan_ids']:
+        invalid_meal_plan_ids = self.test_cases["invalid_meal_plan_ids"]
+        osu_id = invalid_meal_plan_ids["osu_id"]
+        for meal_plan_id in invalid_meal_plan_ids["meal_plan_ids"]:
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}/{meal_plan_id}',
-                'ErrorObject',
+                f"/persons/{osu_id}{endpoint}/{meal_plan_id}",
+                "ErrorObject",
                 404,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
     def test_get_addresses(self):
         """Test case: GET /persons/{osuId}/addresses"""
-
-        endpoint = '/addresses'
-        resource = 'AddressResource'
+        endpoint = "/addresses"
+        resource = "AddressResource"
         nullable_fields = self.get_nullable_fields(resource)
-        valid_person_ids = self.test_cases['valid_person_ids']
+        valid_person_ids = self.test_cases["valid_person_ids"]
 
         for person in valid_person_ids:
-            osu_id = person['osu_id']
+            osu_id = person["osu_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}',
+                f"/persons/{osu_id}{endpoint}",
                 resource,
                 200,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
-        query_params = self.query_params['addresses']
-        osu_id = self.query_params['osu_id']
+        query_params = self.query_params["addresses"]
+        osu_id = self.query_params["osu_id"]
         self.check_query_params(
-            f'/persons/{osu_id}{endpoint}',
+            f"/persons/{osu_id}{endpoint}",
             resource,
             nullable_fields,
             query_params,
-            osu_id
+            osu_id,
         )
 
     def test_get_phones(self):
         """Test case: GET /persons/{osuId}/phones"""
-
-        endpoint = '/phones'
-        resource = 'PhoneResource'
+        endpoint = "/phones"
+        resource = "PhoneResource"
         nullable_fields = self.get_nullable_fields(resource)
-        valid_person_ids = self.test_cases['valid_person_ids']
+        valid_person_ids = self.test_cases["valid_person_ids"]
 
         for person in valid_person_ids:
-            osu_id = person['osu_id']
+            osu_id = person["osu_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}',
+                f"/persons/{osu_id}{endpoint}",
                 resource,
                 200,
-                nullable_fields=nullable_fields
+                nullable_fields=nullable_fields,
             )
 
-        query_params = self.query_params['phones']
-        osu_id = self.query_params['osu_id']
+        query_params = self.query_params["phones"]
+        osu_id = self.query_params["osu_id"]
         self.check_query_params(
-            f'/persons/{osu_id}{endpoint}',
+            f"/persons/{osu_id}{endpoint}",
             resource,
             nullable_fields,
             query_params,
-            osu_id
+            osu_id,
         )
 
     def test_get_emails(self):
         """Test case: GET /persons/{osuId}/emails"""
-
-        endpoint = '/emails'
-        resource = 'EmailResource'
+        endpoint = "/emails"
+        resource = "EmailResource"
         nullable_fields = self.get_nullable_fields(resource)
-        valid_person_ids = self.test_cases['valid_person_ids']
+        valid_person_ids = self.test_cases["valid_person_ids"]
 
         for person in valid_person_ids:
-            osu_id = person['osu_id']
+            osu_id = person["osu_id"]
             self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}',
+                f"/persons/{osu_id}{endpoint}",
                 resource,
                 200,
                 nullable_fields=nullable_fields,
             )
 
-        query_params = self.query_params['emails']
-        osu_id = self.query_params['osu_id']
+        query_params = self.query_params["emails"]
+        osu_id = self.query_params["osu_id"]
         self.check_query_params(
-            f'/persons/{osu_id}{endpoint}',
+            f"/persons/{osu_id}{endpoint}",
             resource,
             nullable_fields,
             query_params,
-            osu_id
-        )
-
-    def test_get_medical(self):
-        """Test case: GET /persons/{osuId}/medical"""
-
-        endpoint = '/medical'
-        resource = 'MedicalResource'
-        nullable_fields = self.get_nullable_fields(resource)
-        valid_person_ids = self.test_cases['valid_person_ids']
-
-        for person in valid_person_ids:
-            osu_id = person['osu_id']
-            self.check_endpoint(
-                f'/persons/{osu_id}{endpoint}',
-                resource,
-                200,
-                nullable_fields=nullable_fields,
-            )
-
-        query_params = self.query_params['medical']
-        osu_id = self.query_params['osu_id']
-        self.check_query_params(
-            f'/persons/{osu_id}{endpoint}',
-            resource,
-            nullable_fields,
-            query_params,
-            osu_id
+            osu_id,
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arguments, argv = utils.parse_arguments()
 
     # Setup logging level
