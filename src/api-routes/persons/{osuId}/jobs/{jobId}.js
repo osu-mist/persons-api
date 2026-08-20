4ad0efafd4ebf2648f95abb700499f6ec975cc37
Shashank Sanjay Betawar <157264361+betawars@users.noreply.github.com>
2026-08-20 11:38:18 -0700

    Onid usage added for jobs endpoint only

--- src/api-routes/persons/{osuId}/jobs/{jobId}.js
+++ src/api-routes/persons/{osuId}/jobs/{jobId}.js
@@ -1,5 +1,5 @@
 import { getJobByJobId, handleJob } from 'db/oracledb/jobs-dao';
-import { personExists } from 'db/oracledb/persons-dao';
+import { personExists, getPersonIdByOnid } from 'db/oracledb/persons-dao';
 import { errorHandler, errorBuilder } from 'errors/errors';
 import { serializeJob, serializePostOrPatch } from 'serializers/jobs-serializer';
 
