7e75043a8de5a1a72e3bfe34843b83f0b4883cb2
betawars <betawars@oregonstate.edu>
2026-08-20 15:06:17 -0700

    test commit

--- src/api-routes/persons.js
+++ src/api-routes/persons.js
@@ -15,6 +15,7 @@ const post = async (req, res) => {
 
     if (moment().isBefore(attributes.birthDate)) {
       return errorBuilder(res, 400, ["birthDate can't be a future date"]);
+      // hsdf
     }
 
     const result = await createPerson(attributes);
